3432fbf7764f61c856f8c20fa6aecc1d9af85fb2
gdov1012 <valerogiovanna7@gmail.com>
2025-08-20 19:23:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+//confere se o navegador suporta o sw 
+if ('serviceWorker' in navigator) {
+    //registro do sw
+    navigator.serviceWorker.register('sw.js')
+        .then(() => console.log('Service Worker registrado com sucesso!'))
+        .catch(err => console.log('Erro ao registrar Service Worker:', err));
+}
---
+++ sw.js
@@ -0,0 +1,16 @@
+
+//self refere-se ao próprio sw 
+//instalou o sw
+self.addEventListener('install', event => {
+    console.log('Service Worker instalado');
+});
+ 
+self.addEventListener('activate', event => {
+    console.log('Service Worker ativado');
+});
+ 
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => new Response("Você está offline"))
+    );
+});
